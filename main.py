249c5010bb52e4a737c122454efc4ce44d0072c6
oAndreAguiar <andre.fel63@gmail.com>
2023-11-22 21:31:20 -0300

    Avanço na criação. Separar os menus pro funções.

--- main.py
+++ main.py
@@ -1,45 +1,75 @@
 import pandas as pd
 
-print('Seja Bem vindo ao seu gerenciador!\n Escolha uma opção')
-print('1.Estoque \n 2.Venda')
+def pesquisar_produto_csv(file_path, termo_pesquisa):
+    df = pd.read_csv(file_path)
+    resultado = df[df['nome'].str.contains(termo_pesquisa, case=False)]
+    return resultado
 
-comando__entrada = int(input('Digite o comando:'))
+def exportar_dict_para_csv(dicionario, nome_arquivo):
+        df = pd.DataFrame.from_dict(dicionario)    
+        df.to_csv(nome_arquivo, index=False)
 
-if comando__entrada == 1:
+
+print('Seja Bem-vindo ao seu gerenciador!\nEscolha uma opção')
+print('1. Estoque\n2. Venda')
+
+comando_entrada = int(input('Digite o comando: '))
+
+
+
+if comando_entrada == 1:
     print('ESTOQUE')
     print('1.Cadastrar produto\n2.Pesquisar\n3.Exportar\n4.Voltar')
-    comando__estoque = input('Escolha uma opção:')
-elif comando__entrada == 2:
+    comando_estoque = int(input('Escolha uma opção:'))
+elif comando_entrada == 2:
     print('VENDA')
     print('1. Criar venda\n2.Editar produto\n3.Precificar\n4.Voltar')
-    comando__venda = input('Escolha uma opção:')
+    comando_venda = int(input('Escolha uma opção:'))
 
-if comando__estoque == 1:
+if comando_estoque == 1:
     estoque = {}
     estoque_nome = []
     estoque_quantidade = []
     estoque_valor = []
     estoque_descricao = []
     while True:
-        estoque_nome.append = str(input('Digite o nome'))
+        nome_produto = input('Digite o nome: ')
+        estoque_nome.append(nome_produto)
         estoque['nome'] = estoque_nome
 
-        estoque_quantidade.append = int(input('Digite a quantidade'))
+        quantidade_produto = int(input('Digite a quantidade: '))
+        estoque_quantidade.append(quantidade_produto)
         estoque['quantidade'] = estoque_quantidade
 
-        estoque_valor.append = float(input('Digite o valor'))
+        valor_produto = float(input('Digite o valor: '))
+        estoque_valor.append(valor_produto)
         estoque['valor'] = estoque_valor
 
-        estoque_descricao.append = str(input('Digite a descrição'))
+        descricao_produto = str(input('Digite a descrição: '))
+        estoque_descricao.append(descricao_produto)
         estoque['descricao'] = estoque_descricao
 
         comando_cadastro = int(input('digite 1 para cadastrar mais um item ou 0 para sair'))
 
         if comando_cadastro == 0:
             estoque = pd.DataFrame.from_dict(estoque)
+            exportar_dict_para_csv(estoque,'estoque.csv')
             print(estoque)
-            break #ADICIONAR FUNÇÃO DE EXPORTAR EM .CSV
+            break 
         elif comando_cadastro == 1:
             pass
-elif comando__estoque == 2:
-    
+elif comando_estoque == 2:
+    df_estoque = pd.read_csv('estoque.csv')
+
+    termo_pesquisa = input('Digite o termo de pesquisa: ')
+    resultado_pesquisa = pesquisar_produto_csv('estoque.csv', termo_pesquisa)
+
+    if not resultado_pesquisa.empty:
+            print(resultado_pesquisa)
+    else:
+        print('Nenhum resultado encontrado.')
+elif comando_estoque == 3:
+    exportar_dict_para_csv(meu_dict, 'output.csv')
+
+elif comando_estoque == 4:
+    pass
